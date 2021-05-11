0d4bd4f40927d1a9e40f139a49b47483a441387a
Melvin Strobl <melvin.strobl@web.de>
2021-05-11 17:13:54 +0200

    qft exp

--- qft.py
+++ qft.py
@@ -1,3 +1,10 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
+
+
+# %%
 import numpy as np
 from numpy import pi
 import matplotlib.pyplot as plt
@@ -6,8 +13,14 @@ from qiskit import *
 from qiskit.visualization import plot_histogram
 
 plt.style.use('seaborn-poster')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
+# %% [markdown]
+# # Classical (D)FT
+# 
+# First let's do a classical dft on a toy-signal to get familiar again with this whole signal processing topic
 
+# %%
 def DFT(x):
     """
     Function to calculate the 
@@ -25,14 +38,15 @@ def DFT(x):
     return X
 
 
+# %%
 # sampling rate
 sr = 100
 # sampling interval
 ts = 1.0/sr
 t = np.arange(0,1,ts)
 
-freq = 1.
-x = 3*np.sin(2*np.pi*freq*t)
+freq = 2.
+x = np.sin(2*np.pi*freq*t)
 
 plt.figure(figsize = (8, 6))
 plt.plot(t, x, 'r')
@@ -41,6 +55,8 @@ plt.xlabel('Normalized Time')
 
 plt.show()
 
+
+# %%
 X = DFT(x)
 
 # calculate the frequency
@@ -49,6 +65,8 @@ n = np.arange(N)
 T = N/sr
 freq = n/T 
 
+
+# %%
 n_oneside = N//2
 # get the one side frequency
 f_oneside = freq[:n_oneside]
@@ -58,21 +76,23 @@ X_oneside =X[:n_oneside]/n_oneside
 
 plt.figure(figsize = (12, 6))
 plt.subplot(121)
-plt.stem(f_oneside, abs(X_oneside), 'b', \
-         markerfmt=" ", basefmt="-b")
+plt.stem(f_oneside, abs(X_oneside), 'b',          markerfmt=" ", basefmt="-b")
 plt.xlabel('Freq (Hz)')
 plt.ylabel('DFT Amplitude |X(freq)|')
 
 plt.subplot(122)
-plt.stem(f_oneside, abs(X_oneside), 'b', \
-         markerfmt=" ", basefmt="-b")
+plt.stem(f_oneside, abs(X_oneside), 'b',          markerfmt=" ", basefmt="-b")
 plt.xlabel('Freq (Hz)')
 plt.xlim(0, 10)
 plt.tight_layout()
 plt.show()
 
+# %% [markdown]
+# # Quantum FT
+# 
+# Next, we let's try to achieve the same result with the qft circuit
 
-
+# %%
 def qft_rotations(circuit, n):
     """Performs qft on the first n qubits in circuit (without swaps)"""
     if n == 0:
@@ -94,7 +114,8 @@ def swap_registers(circuit, n):
 
 def measure(circuit, n):
     for qubit in range(n):
-        circuit.measure(qubit, 0)
+        circuit.barrier(qubit)
+        circuit.measure_all()
 
 def qft(circuit, n):
     """QFT on the first n qubits in circuit"""
@@ -103,45 +124,120 @@ def qft(circuit, n):
     measure(circuit,n)
     return circuit
 
-SCALER = 1
+
+# %%
+
 def preprocessSignal(signal):
     signal = signal*SCALER
     signal = signal + abs(min(signal))
 
     return signal
 
+
+# %%
 #Parameters:
+SCALER = 10
 
 signal = preprocessSignal(x)
 # x_processed = x_processed[2:4]
 CIRCUIT_SIZE = int(max(signal)).bit_length() # this basically defines the "adc resolution"
-print(CIRCUIT_SIZE)
+print(f"Using Scaler {SCALER} resulted in Circuit Size {CIRCUIT_SIZE}")
 
-def encodeInteger(encIntegerCircuit, integer):
-    if integer.bit_length() > encIntegerCircuit.width():
+
+# %%
+# qc = QuantumCircuit(CIRCUIT_SIZE)
+# qft(qc,CIRCUIT_SIZE)
+# qc.draw()
+
+
+# %%
+def encodeInteger(circuit, integer):
+    if integer.bit_length() > circuit.width():
         raise RuntimeError("Integer too large")
 
     binary = bin(integer)[2:]
-    for i in range(0,min(len(binary),encIntegerCircuit.width()-1)):
+    for i in range(0,min(len(binary),circuit.width()-1)):
         if binary[i] == '1':
-            encIntegerCircuit.x(i)
+            circuit.x(i)
 
-    return encIntegerCircuit
+    return circuit
+
+def runCircuit(circuit):
+    backend = Aer.get_backend("statevector_simulator")
+    qobj = assemble(circuit)
+    output = backend.run(qobj).result().get_statevector()
+    
+    return output
+        
+def decodeInteger(integer):
+    return int(integer,2)
+
+def accumulate(buffer, value):
+    buffer[value] += 1
+
+    return buffer
 
 def processQFT(circuit, signal):
-    result = list()
+    signal_hat = np.zeros(signal.size)
 
     for i in range(0,signal.size):
-        circuit = QuantumCircuit(CIRCUIT_SIZE, 1)
+        circuit = QuantumCircuit(CIRCUIT_SIZE, CIRCUIT_SIZE)
         circuit.reset(range(CIRCUIT_SIZE))
 
-        qft(circuit,CIRCUIT_SIZE)
         circuit = encodeInteger(circuit, int(signal[i]))
+        qft(circuit,CIRCUIT_SIZE)
+        
 
-        backend = Aer.get_backend("qasm_simulator")
-        job = execute(circuit, backend, shots=1, memory=True)
-        output = job.result().get_memory()[0]
+        output = runCircuit(circuit)
+        
+        signal_hat = accumulate(signal_hat, output.argmax(axis=0))
 
-        print(f"Processing index {i} with value {int(signal[i])} yielded {output}")
+        print(f"Processing index {i} with value {int(signal[i])} yielded {output.argmax(axis=0)}")
 
-circuit = processQFT(circuit, signal)
+    return signal_hat
+
+
+# %%
+i=0
+circuit = QuantumCircuit(CIRCUIT_SIZE, CIRCUIT_SIZE)
+circuit.reset(range(CIRCUIT_SIZE))
+
+circuit = encodeInteger(circuit, int(signal[i]))
+qft(circuit,CIRCUIT_SIZE)
+
+backend = Aer.get_backend("qasm_simulator")
+job = execute(circuit, backend, shots=1, memory=True)
+output = job.result().get_memory()[0]
+# circuit.draw()
+
+
+# %%
+signal_hat = processQFT(circuit, signal)
+
+
+# %%
+n_oneside = N//2
+# get the one side frequency
+f_oneside = freq[:n_oneside]/(2*np.pi)
+
+# normalize the amplitude
+X_oneside =signal_hat[:n_oneside]/n_oneside
+
+plt.figure(figsize = (12, 6))
+plt.subplot(121)
+plt.stem(f_oneside, abs(X_oneside), 'b', markerfmt=" ", basefmt="-b")
+plt.xlabel('Normalized Freq (Hz)')
+plt.ylabel('DFT Amplitude |X(freq)|')
+
+plt.subplot(122)
+plt.stem(f_oneside, abs(X_oneside), 'b', markerfmt=" ", basefmt="-b")
+plt.xlabel('Freq (Hz)')
+plt.xlim(0, 10)
+plt.tight_layout()
+plt.show()
+
+
+# %%
+
+
+
