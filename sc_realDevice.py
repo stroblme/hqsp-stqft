ceebd0a8ab2a4a3bee324b0060d0a5b0e3117024
stroblme <melvin.strobl@web.de>
2021-06-28 16:03:57 +0200

    evaluation in config

--- sc_realDevice.py
+++ sc_realDevice.py
@@ -1,10 +1,13 @@
-from matplotlib.pyplot import draw
+from matplotlib.pyplot import draw, text
+from numpy.random import random
+from qiskit.providers import backend
 from qft import qft_framework
 from dft import dft_framework
 from fft import fft_framework
 from stft import stft_framework
 from stqft import stqft_framework
 from frontend import grader, signal, transform, primeTime, enableInteractive
+from utils import PI
 
 enableInteractive()
 
@@ -13,27 +16,62 @@ print("Initializing Harmonic Signal")
 y = signal(samplingRate=1000, amplification=1, duration=0, nSamples=2**5)
 
 y.addFrequency(250) # you should choose a frequency matching a multiple of samplingRate/nSamples
-# y.addFrequency(2000)
 
-y.show(subplot=[1,3,1])
+y.show(subplot=[2,9,1])
 
-print("Processing Simulated QFT")
+print("Processing FFT")
 
-qft = transform(qft_framework, minRotation=0, suppressPrint=False, simulation=True)
-y_hat, f = qft.forward(y)
-y_hat_sim, f_p = qft.postProcess(y_hat, f)
-qft.show(y_hat_sim, f_p, subplot=[1,3,2])
+fft = transform(fft_framework)
+y_hat, f = fft.forward(y)
+y_hat_ideal, f_p = fft.postProcess(y_hat, f)
+fft.show(y_hat_ideal, f_p, subplot=[2,9,10], title="FFT (ref)")
+
 
 print("Processing Real QFT")
 
-qft = transform(qft_framework, minRotation=0, suppressPrint=False, simulation=False)
-y_hat, f = qft.forward(y)
-y_hat_real, f_p = qft.postProcess(y_hat, f)
-qft.show(y_hat_real, f_p, subplot=[1,3,3])
-
+mrot = 0
+pt = 0
 grader_inst = grader()
-snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_sim)
-print(f"Calculated snr of {snr}db")
+import random
+while mrot <= PI/2:
+    qft = transform(qft_framework, minRotation=mrot, suppressPrint=False, backend="simu")
+    y_hat, f = qft.forward(y)
+    y_hat_real, f_p = qft.postProcess(y_hat, f)
+    ylabel = "Amplitude" if pt == 0 else " "
+    qft.show(y_hat_real, f_p, subplot=[2,9,pt+3], title=f"QFT_sim, mr:{mrot:.2f}", xlabel=" ", ylabel=ylabel)
+    snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_ideal)
+    print(f"Calculated an snr of {snr} db")
+    grader_inst.log(snr, mrot)
+    print(f"Minimum rotation is: {mrot}")
+    pt += 1
+    mrot = PI/2**(5-pt)
+
+grader_inst.show(subplot=[2,9,9])
+
+
+mrot = 0
+pt = 0
+grader_inst = grader()
+import random
+while mrot <= PI/2:
+    qft = transform(qft_framework, minRotation=mrot, suppressPrint=False, backend="melbourne")
+    y_hat, f = qft.forward(y)
+    y_hat_real, f_p = qft.postProcess(y_hat, f)
+    ylabel = "Amplitude" if pt == 0 else " "
+    qft.show(y_hat_real, f_p, subplot=[2,9,pt+12], title=f"QFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel=ylabel)
+    snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_ideal)
+    print(f"Calculated an snr of {snr} db")
+    grader_inst.log(snr, mrot)
+    print(f"Minimum rotation is: {mrot}")
+    pt += 1
+    mrot = PI/2**(5-pt)
+
+grader_inst.show(subplot=[2,9,18])
+
+
+# grader_inst = grader()
+# snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_sim)
+# print(f"Calculated an snr of {snr} db")
 # windowLength = 2**5 #only 5 qubits allowed
 # overlapFactor=0.5
 # windowType='hann'
