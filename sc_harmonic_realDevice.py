0a75a952201efc3d42be167f1c3f0d63da5bc9f4
stroblme <melvin.strobl@web.de>
2021-07-06 17:59:49 +0200

    naming

--- sc_harmonic_realDevice.py
+++ sc_harmonic_realDevice.py
@@ -21,12 +21,12 @@ y = signal(samplingRate=1000, amplification=1, duration=0, nSamples=2**4)
 y.addFrequency(250) # you should choose a frequency matching a multiple of samplingRate/nSamples
 y.addFrequency(125) # you should choose a frequency matching a multiple of samplingRate/nSamples
 
-figHandle = y.show(subplot=[2,9,1])
+plotData = y.show(subplot=[2,9,1])
 
 exp = export(topic=TOPIC, identifier="signal")
-exp.setData(export.SIGNAL, y)
+# exp.setData(export.SIGNAL, y)
 exp.setData(export.DESCRIPTION, "Harmonic Signal, 125 and 250 Hz at 1kHz, 2^4 samples")
-exp.setData(export.PLOTHANDLE, figHandle)
+exp.setData(export.PLOTHANDLE, plotData)
 exp.doExport()
 
 print("Processing FFT")
@@ -34,12 +34,12 @@ print("Processing FFT")
 fft = transform(fft_framework)
 y_hat, f = fft.forward(y)
 y_hat_ideal, f_p = fft.postProcess(y_hat, f)
-figHandle = fft.show(y_hat_ideal, f_p, subplot=[2,9,10], title="FFT (ref)")
+plotData = fft.show(y_hat_ideal, f_p, subplot=[2,9,10], title="FFT (ref)")
 
 exp = export(topic=TOPIC, identifier="fft")
 exp.setData(export.SIGNAL, y_hat_ideal)
 exp.setData(export.DESCRIPTION, "FFT, default param, post processed")
-exp.setData(export.PLOTHANDLE, figHandle)
+exp.setData(export.PLOTHANDLE, plotData)
 exp.doExport()
 
 print("Processing Simulated QFT")
@@ -53,7 +53,7 @@ while mrot <= PI/2:
     y_hat, f = qft.forward(y)
     y_hat_real, f_p = qft.postProcess(y_hat, f)
     ylabel = "Amplitude" if pt == 0 else " "
-    figHandle = qft.show(y_hat_real, f_p, subplot=[2,9,pt+3], title=f"QFT_sim, mr:{mrot:.2f}", xlabel=" ", ylabel=ylabel)
+    plotData = qft.show(y_hat_real, f_p, subplot=[2,9,pt+3], title=f"QFT_sim, mr:{mrot:.2f}", xlabel=" ", ylabel=ylabel)
     snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_ideal)
     print(f"Calculated an snr of {snr} db")
     grader_inst.log(snr, mrot)
@@ -64,16 +64,16 @@ while mrot <= PI/2:
     exp = export(topic=TOPIC, identifier=f"qft_sim_mr_{mrot:.2f}")
     exp.setData(export.SIGNAL, y_hat_real)
     exp.setData(export.DESCRIPTION, f"QFT, simulated, mrot={mrot}, post processed")
-    exp.setData(export.PLOTHANDLE, figHandle)
+    exp.setData(export.PLOTHANDLE, plotData)
     exp.doExport()
 
-figHandle = grader_inst.show(subplot=[2,9,9])
+plotData = grader_inst.show(subplot=[2,9,9])
 
 exp = export(topic=TOPIC, identifier="grader_qft_sim")
 exp.setData(export.GRADERX, grader_inst.xValues)
 exp.setData(export.GRADERY, grader_inst.yValues)
 exp.setData(export.DESCRIPTION, "Grader, qft_sim")
-exp.setData(export.PLOTHANDLE, figHandle)
+exp.setData(export.PLOTHANDLE, plotData)
 exp.doExport()
 
 print("Processing Real QFT")
@@ -87,7 +87,7 @@ while mrot <= PI/2:
     y_hat, f = qft.forward(y)
     y_hat_real, f_p = qft.postProcess(y_hat, f)
     ylabel = "Amplitude" if pt == 0 else " "
-    figHandle = qft.show(y_hat_real, f_p, subplot=[2,9,pt+12], title=f"QFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel=ylabel)
+    plotData = qft.show(y_hat_real, f_p, subplot=[2,9,pt+12], title=f"QFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel=ylabel)
     snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_ideal)
     print(f"Calculated an snr of {snr} db")
     grader_inst.log(snr, mrot)
@@ -98,16 +98,16 @@ while mrot <= PI/2:
     exp = export(topic=TOPIC, identifier=f"qft_real_mr_{mrot:.2f}")
     exp.setData(export.SIGNAL, y_hat_real)
     exp.setData(export.DESCRIPTION, f"QFT, simulated, ibmq_quito noise, mrot={mrot}, post processed")
-    exp.setData(export.PLOTHANDLE, figHandle)
+    exp.setData(export.PLOTHANDLE, plotData)
     exp.doExport()
 
-figHandle = grader_inst.show(subplot=[2,9,18])
+plotData = grader_inst.show(subplot=[2,9,18])
 
 exp = export(topic=TOPIC, identifier="grader_qft_real")
 exp.setData(export.GRADERX, grader_inst.xValues)
 exp.setData(export.GRADERY, grader_inst.yValues)
 exp.setData(export.DESCRIPTION, "Grader, qft_real")
-exp.setData(export.PLOTHANDLE, figHandle)
+exp.setData(export.PLOTHANDLE, plotData)
 exp.doExport()
 
 # grader_inst = grader()
