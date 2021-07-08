79e004239f4b5b7cb7bffba1cb52583030b32eee
stroblme <melvin.strobl@web.de>
2021-07-08 17:54:10 +0200

    minor fixes

--- sc_minRot_chirp.py
+++ sc_minRot_chirp.py
@@ -51,12 +51,11 @@ grader_inst = grader()
 
 while mrot <= PI/2:
     stqft = transform(stqft_framework, minRotation=mrot, suppressPrint=False)
-    stqft = transform(stqft_framework, minRotation=0.2, suppressPrint=True, backend="simu")
     y_hat_sim, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
     y_hat_sim_p, f_p, t_p = stqft.postProcess(y_hat_sim, f ,t, scale='mel')
     plotData = stqft.show(y_hat_sim_p, f_p, t_p, subplot=[1,4,3])
 
-    snr = grader_inst.calculateNoisePower(y_hat_sim_p, y_hat_stft)
+    snr = grader_inst.calculateNoisePower(y_hat_sim_p, y_hat_stft_p)
     print(f"Calculated an snr of {snr} db")
     grader_inst.log(snr, mrot)
     print(f"Minimum rotation is: {mrot}")
@@ -90,7 +89,7 @@ while mrot <= PI/2:
     y_hat_real_p, f_p = stqft.postProcess(y_hat, f)
     ylabel = "Amplitude" if pt == 0 else " "
     plotData = stqft.show(y_hat_real_p, f_p, subplot=[2,9,pt+12], title=f"STQFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel=ylabel)
-    snr = grader_inst.calculateNoisePower(y_hat_real_p, y_hat_stft)
+    snr = grader_inst.calculateNoisePower(y_hat_real_p, y_hat_stft_p)
     print(f"Calculated an snr of {snr} db")
     grader_inst.log(snr, mrot)
     print(f"Minimum rotation is: {mrot}")
