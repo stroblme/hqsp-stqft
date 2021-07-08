da5b74ebd8b13e79d0047c405af61978e0bcd876
stroblme <melvin.strobl@web.de>
2021-07-08 16:34:35 +0200

    mv to speech

--- sc_speech_realDevice.py
+++ sc_speech_realDevice.py
@@ -4,9 +4,12 @@ from dft import dft_framework
 from fft import fft_framework
 from stft import stft_framework
 from stqft import stqft_framework
-from frontend import frontend, signal, transform
+from frontend import frontend, signal, transform, export
+from utils import PI
 
 frontend.enableInteractive()
+TOPIC = "speech_realDevice"
+export.checkWorkingTree()
 
 print("Initializing Harmonic Signal")
 
@@ -15,16 +18,30 @@ y = signal(samplingRate=16000, amplification=1, duration=1, nSamples=2**16)
 y.addFrequency(800)
 y.addFrequency(2000)
 
-y.show(subplot=[2,2,1])
+plotData = y.show(subplot=[2,2,1])
+
+exp = export(topic=TOPIC, identifier="signal")
+# exp.setData(export.SIGNAL, y)
+exp.setData(export.DESCRIPTION, "Harmonic Signal, 125 and 250 Hz at 1kHz, 2^4 samples")
+exp.setData(export.PLOTDATA, plotData)
+exp.doExport()
 
 print("Processing QFT")
 
 qft = transform(qft_framework, minRotation=0.2, suppressPrint=False)
 y_hat, f = qft.forward(y)
 y_hat_p, f_p = qft.postProcess(y_hat, f)
-qft.show(y_hat_p, f_p, subplot=[2,2,2])
+plotData = qft.show(y_hat_p, f_p, subplot=[2,2,2])
 
-windowLength = 2**7
+exp = export(topic=TOPIC, identifier="fft")
+exp.setData(export.SIGNAL, y_hat_ideal)
+exp.setData(export.DESCRIPTION, "FFT, default param, post processed")
+exp.setData(export.PLOTDATA, plotData)
+exp.doExport()
+
+nQubits = 7
+bandwidth = 4
+windowLength = 2**nQubits
 overlapFactor=0.5
 windowType='hann'
 
@@ -43,7 +60,7 @@ y.addFrequency(3000, y.duration)
 y.show(subplot=[2,2,3])
 
 print("Processing STQFT")
-stqft = transform(stqft_framework, minRotation=0.2, suppressPrint=False, draw=True)
+stqft = transform(stqft_framework, minRotation=PI/2**(nQubits-bandwidth), suppressPrint=False, draw=True)
 y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
 y_hat_sqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel')
 stqft.show(y_hat_sqft_p, f_p, t_p, subplot=[2,2,4])
