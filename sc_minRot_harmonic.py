08b068b95918d19e22defc15b7cfac69eb19489e
stroblme <melvin.strobl@web.de>
2021-07-08 17:07:33 +0200

    renamed

--- sc_minRot_harmonic.py
+++ sc_minRot_harmonic.py
@@ -27,11 +27,11 @@ print("Processing FFT")
 
 fft = transform(fft_framework)
 y_hat, f = fft.forward(y)
-y_hat_ideal, f_p = fft.postProcess(y_hat, f)
-plotData = fft.show(y_hat_ideal, f_p, subplot=[2,9,10], title="FFT (ref)")
+y_hat_ideal_p, f_p = fft.postProcess(y_hat, f)
+plotData = fft.show(y_hat_ideal_p, f_p, subplot=[2,9,10], title="FFT (ref)")
 
 exp = export(topic=TOPIC, identifier="fft")
-exp.setData(export.SIGNAL, y_hat_ideal)
+exp.setData(export.SIGNAL, y_hat_ideal_p)
 exp.setData(export.DESCRIPTION, "FFT, default param, post processed")
 exp.setData(export.PLOTDATA, plotData)
 exp.doExport()
@@ -45,11 +45,11 @@ grader_inst = grader()
 while mrot <= PI/2:
     qft = transform(qft_framework, minRotation=mrot, suppressPrint=False)
     y_hat, f = qft.forward(y)
-    y_hat_real, f_p = qft.postProcess(y_hat, f)
+    y_hat_sim_p, f_p = qft.postProcess(y_hat, f)
     ylabel = "Amplitude" if pt == 0 else " "
-    plotData = qft.show(y_hat_real, f_p, subplot=[2,9,pt+3], title=f"QFT_sim, mr:{mrot:.2f}", xlabel=" ", ylabel=ylabel)
+    plotData = qft.show(y_hat_sim_p, f_p, subplot=[2,9,pt+3], title=f"QFT_sim, mr:{mrot:.2f}", xlabel=" ", ylabel=ylabel)
 
-    snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_ideal)
+    snr = grader_inst.calculateNoisePower(y_hat_sim_p, y_hat_ideal_p)
     print(f"Calculated an snr of {snr} db")
     grader_inst.log(snr, mrot)
     print(f"Minimum rotation is: {mrot}")
@@ -57,7 +57,7 @@ while mrot <= PI/2:
     mrot = PI/2**(5-pt)
 
     exp = export(topic=TOPIC, identifier=f"qft_sim_mr_{mrot:.2f}")
-    exp.setData(export.SIGNAL, y_hat_real)
+    exp.setData(export.SIGNAL, y_hat_sim_p)
     exp.setData(export.DESCRIPTION, f"QFT, simulated, mrot={mrot}, post processed")
     exp.setData(export.PLOTDATA, plotData)
     exp.doExport()
@@ -80,10 +80,10 @@ import random
 while mrot <= PI/2:
     qft = transform(qft_framework, minRotation=mrot, suppressPrint=False, simulation=True, backendName="ibmq_quito")
     y_hat, f = qft.forward(y)
-    y_hat_real, f_p = qft.postProcess(y_hat, f)
+    y_hat_real_p, f_p = qft.postProcess(y_hat, f)
     ylabel = "Amplitude" if pt == 0 else " "
-    plotData = qft.show(y_hat_real, f_p, subplot=[2,9,pt+12], title=f"QFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel=ylabel)
-    snr = grader_inst.calculateNoisePower(y_hat_real, y_hat_ideal)
+    plotData = qft.show(y_hat_real_p, f_p, subplot=[2,9,pt+12], title=f"QFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel=ylabel)
+    snr = grader_inst.calculateNoisePower(y_hat_real_p, y_hat_ideal_p)
     print(f"Calculated an snr of {snr} db")
     grader_inst.log(snr, mrot)
     print(f"Minimum rotation is: {mrot}")
@@ -91,7 +91,7 @@ while mrot <= PI/2:
     mrot = PI/2**(5-pt)
 
     exp = export(topic=TOPIC, identifier=f"qft_real_mr_{mrot:.2f}")
-    exp.setData(export.SIGNAL, y_hat_real)
+    exp.setData(export.SIGNAL, y_hat_real_p)
     exp.setData(export.DESCRIPTION, f"QFT, simulated, ibmq_quito noise, mrot={mrot}, post processed")
     exp.setData(export.PLOTDATA, plotData)
     exp.doExport()
