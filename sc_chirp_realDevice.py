e1a7ba417277dc6b64432a3c44f5cf880f447fd0
stroblme <melvin.strobl@web.de>
2021-07-10 08:34:26 +0200

    title

--- sc_chirp_realDevice.py
+++ sc_chirp_realDevice.py
@@ -6,12 +6,13 @@ from dft import dft_framework
 from fft import fft_framework
 from stft import stft_framework
 from stqft import stqft_framework
-from frontend import grader, signal, transform, primeTime, enableInteractive
+from frontend import frontend, grader, signal, transform, export
+
 from utils import PI
 
-enableInteractive()
+frontend.enableInteractive()
 
-windowLength = 2**7
+windowLength = 2**4
 overlapFactor=0.5
 windowType='hann'
 
@@ -25,25 +26,25 @@ y.addFrequency(2000, y.duration)
 y.addFrequency(1000)
 y.addFrequency(3000, y.duration)
 
-y.show(subplot=[1,4,1])
+y.show(subplot=[1,4,1], title="signal")
 
 print("Processing STFT")
 stft = transform(stft_framework)
 y_hat_stft, f ,t = stft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
 y_hat_stft_p, f_p, t_p = stft.postProcess(y_hat_stft, f ,t)
-stft.show(y_hat_stft_p, f_p, t_p, subplot=[1,4,2])
+stft.show(y_hat_stft_p, f_p, t_p, subplot=[1,4,2], title="stft")
 
 print("Processing simulation STQFT")
-stqft = transform(stqft_framework, minRotation=0.2, suppressPrint=True, backend="simu")
+stqft = transform(stqft_framework, minRotation=0.2, suppressPrint=True)
 y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
 y_hat_sqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel')
-stqft.show(y_hat_sqft_p, f_p, t_p, subplot=[1,4,3])
+stqft.show(y_hat_sqft_p, f_p, t_p, subplot=[1,4,3], title="stqft")
 
-print("Processing real STQFT")
-stqft = transform(stqft_framework, minRotation=0.2, suppressPrint=True, backend="ibmq_16_melbourne")
-y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
-y_hat_sqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel')
-stqft.show(y_hat_sqft_p, f_p, t_p, subplot=[1,4,4])
+# print("Processing real STQFT")
+# stqft = transform(stqft_framework, minRotation=0, suppressPrint=True, simulation=True, backendName="ibmq_casablanca")
+# y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
+# y_hat_sqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel')
+# stqft.show(y_hat_sqft_p, f_p, t_p, subplot=[1,4,4])
 
 print("Showing all figures")
-primeTime() # Show all with blocking
+frontend.primeTime() # Show all with blocking
