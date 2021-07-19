4abd74a52b436aa4abac75c58d1c03b92a901e4a
stroblme <melvin.strobl@web.de>
2021-07-19 10:02:49 +0200

    qiskit filter test

--- sc_qiskitFilter.py
+++ sc_qiskitFilter.py
@@ -1,5 +1,5 @@
-from qft import get_fft_from_counts
-from frontend import signal
+from qft import get_fft_from_counts, loadBackend, qft_framework
+from frontend import frontend, signal, transform
 
 from qiskit.circuit.library import QFT as qiskit_qft
 
@@ -22,39 +22,44 @@ from numpy import linalg as LA
 import numpy as np
 #%config jupy = 'svg' # Makes the images look nice
 
-import time
-
-import matplotlib.pyplot as plt
-
 
 # --- Computation of the calibration matrix
-nBits    = 4
-
-shots    = 2048
 
 from qiskit.ignis.mitigation.measurement import (complete_meas_cal,CompleteMeasFitter)
 from qiskit import *
-from qiskit.circuit.library import QFT
 
-qr = QuantumRegister(nBits)
+
+
+
+nQubits   = 4
+nShots    = 2048
+
+
+
+qr = QuantumRegister(nQubits)
 meas_calibs, state_labels = complete_meas_cal(qr=qr, circlabel='mcal')
-backend = provider.get_backend('ibmq_quito')
+_, backend = loadBackend('ibmq_quito', True)
 job = execute(meas_calibs, backend=backend, shots=1000)
-job_monitor(job, interval = 3)
+# job_monitor(job, interval = 3)
 cal_results = job.result()
 
 meas_fitter = CompleteMeasFitter(cal_results, state_labels, circlabel='mcal')
 print(meas_fitter.cal_matrix)
 
-# --- Execution of the noisy quantum circuit
+
+
+
+
+
 
 q = QuantumRegister(4,'q')
 
 qc = QuantumCircuit(q)
 # Normalize ampl, which is required for squared sum of amps=1
-ys = signal(samplingRate=1000, amplification=1, duration=0, nSamples=2**nBits)
+ys = signal(samplingRate=1000, amplification=1, duration=0, nSamples=2**nQubits)
 
 ys.addFrequency(125)
+ys.addFrequency(250)
 y = ys.sample()
 # y.addFrequency(250)
 ampls = y / np.linalg.norm(y)
@@ -62,20 +67,42 @@ ampls = y / np.linalg.norm(y)
 # for 2^n amplitudes, we have n qubits for initialization
 # this means that the binary representation happens exactly here
 
-qc.initialize(ampls, [q[i] for i in range(nBits)])
+qc.initialize(ampls, [q[i] for i in range(nQubits)])
 
-qc += qiskit_qft(num_qubits=nBits, approximation_degree=0, do_swaps=True, inverse=False, insert_barriers=False, name='qft')
+qc += qiskit_qft(num_qubits=nQubits, approximation_degree=0, do_swaps=True, inverse=False, insert_barriers=False, name='qft')
 
 qc.measure_all()
-qc = transpile(qc, provider.get_backend('ibmq_quito'), optimization_level=1) # opt level 0,1..3. 3: heaviest opt
-job = execute(qc, provider.get_backend('ibmq_quito'), shots = shots)
+qc = transpile(qc, backend, optimization_level=1) # opt level 0,1..3. 3: heaviest opt
+job = execute(qc, backend, shots = nShots)
 #job = execute(qc, BasicAer.get_backend('qasm_simulator'), shots = shots)
 
-job_monitor(job, interval = 3)
 result = job.result()
-print(result.get_counts())
+# print(result.get_counts())
 
-# --- Error correction
+
+
+
+
+genTransform = transform(None)
+
+
+
+
+
+y_hat = np.array(get_fft_from_counts(result.get_counts(), nQubits))
+
+f = genTransform.calcFreqArray(ys, y_hat)
+y_hat_sim_p, f_p = genTransform.postProcess(y_hat, f)
+plotData = genTransform.show(y_hat_sim_p, f_p, subplot=[1,2,1], title=f"qft_sim_n")
+
+print(y_hat)
+
+
+
+
+
+
+
 
 # Get the filter object
 meas_filter = meas_fitter.filter
@@ -84,8 +111,16 @@ meas_filter = meas_fitter.filter
 mitigated_results = meas_filter.apply(result)
 mitigated_counts = mitigated_results.get_counts(0)
 
-print(mitigated_counts)
+# print(mitigated_counts)
 
-y_hat = np.array(get_fft_from_counts(mitigated_counts, nBits))
+
+
+y_hat = np.array(get_fft_from_counts(mitigated_counts, nQubits))
+
+f = genTransform.calcFreqArray(ys, y_hat)
+y_hat_sim_p, f_p = genTransform.postProcess(y_hat, f)
+plotData = genTransform.show(y_hat_sim_p, f_p, subplot=[1,2,2], title=f"qft_sim_n_f")
 
 print(y_hat)
+
+frontend.primeTime()
