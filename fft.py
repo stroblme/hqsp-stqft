788f0bd701ec363fe111b895a4846e3ea6aebf6f
stroblme <melvin.strobl@web.de>
2021-08-03 09:56:36 +0200

    inv transfrom prep

--- fft.py
+++ fft.py
@@ -20,6 +20,12 @@ class fft_framework():
 
         return y_hat
 
+    def transformInv(self, y_signal):
+        y_hat = y_signal.sample()
+        y = self.ifft_recurse(y_hat)
+
+        return y
+
     def postProcess(self, y_hat, f):
         return y_hat, f
 
@@ -35,6 +41,24 @@ class fft_framework():
 
             # Fourier factor
             rotation = np.exp(-2j*np.pi*np.arange(N)/ N)
+            
+            y_hat = np.concatenate([y_hat_even+rotation[:int(N/2)]*y_hat_odd,
+                                    y_hat_even+rotation[int(N/2):]*y_hat_odd])
+
+        return y_hat
+
+    def ifft_recurse(self, y):
+        N = len(y)
+        
+        if N == 1:
+            y_hat = y
+        else:
+            # Get every second sample starting from zero and one (even and odd samples)
+            y_hat_even = self.fft_recurse(y[::2])
+            y_hat_odd = self.fft_recurse(y[1::2])
+
+            # Fourier factor
+            rotation = np.exp(+2j*np.pi*np.arange(N)/ N)
             
             y_hat = np.concatenate([y_hat_even+rotation[:int(N/2)]*y_hat_odd,
                                     y_hat_even+rotation[int(N/2):]*y_hat_odd])
