a1b57ee655744129025a7a928b99b4c809f89ef5
stroblme <melvin.strobl@web.de>
2021-08-05 14:57:04 +0200

    adaption

--- sc_inverseTransform.py
+++ sc_inverseTransform.py
@@ -20,7 +20,8 @@ mrot = 0.2
 
 print("Initializing Chirp Signal")
 
-y = signal(samplingRate=8000, amplification=1, duration=3, nSamples=0, signalType='chirp')
+y = signal(samplingRate=8000, amplification=1, duration=0, nSamples=2**12, signalType='chirp')
+# y = signal(samplingRate=8000, amplification=1, duration=3, nSamples=0, signalType='chirp')
 # speechSignal = '../dataset/zero/4634529e_nohash_2.wav'  #female noise
 
 # y = signal(samplingRate=16000, signalType='file', path=speechSignal)
@@ -38,10 +39,10 @@ plotData = y.show(subplot=[2,2,1])
 
 print("Processing STFT")
 
-stft = transform(stft_framework)
-y_hat_stft, f ,t = stft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
-y_hat_stft_p, f_p, t_p = stft.postProcess(y_hat_stft, f ,t)
-plotData = stft.show(y_hat_stft_p, f_p, t_p, subplot=[2,2,3])
+# stft = transform(stft_framework)
+# y_hat_stft, f ,t = stft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
+# y_hat_stft_p, f_p, t_p = stft.postProcess(y_hat_stft, f ,t)
+# plotData = stft.show(y_hat_stft_p, f_p, t_p, subplot=[2,2,3])
 
 print("Processing Simulated STQFT")
 
@@ -49,24 +50,34 @@ print("Processing Simulated STQFT")
 stqft = transform(stqft_framework, minRotation=mrot, suppressPrint=True)
 y_hat_sim, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
 y_hat_sim_p, f_p, t_p = stqft.postProcess(y_hat_sim, f ,t, scale='mel')
-plotData = stqft.show(y_hat_sim_p, f_p, t_p, subplot=[2,2,4], title=f"STQFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel="Amplitude")
+plotData = stqft.show(y_hat_sim_p, f_p, t_p, subplot=[2,2,3], title=f"STQFT_real, mr:{mrot:.2f}",  xlabel="Freq (Hz)", ylabel="Amplitude")
 
 print("Processing Inverse STFT")
 
 y_i = y
 
 y_hat = signal()
-y_hat.externalSample(y_hat_sim_p, t, f)
+y_hat.externalSample(y_hat_sim, t, f)
 
 stft = transform(stft_framework, transform=ifft_framework)
 y_i_samples, t = stft.backward(y_hat, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
 y_i.externalSample(y_i_samples,y.t)
 y_i.show(subplot=[2,2,2])
 
+
+print("Processing STFT")
+
+y_inv = y
+y_inv.externalSample(y_i_samples, y.t)
+
+stft = transform(stft_framework)
+y_hat_stft, f ,t = stft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
+y_hat_stft_p, f_p, t_p = stft.postProcess(y_hat_stft, f ,t)
+plotData = stft.show(y_hat_stft_p, f_p, t_p, subplot=[2,2,4])
+
+# import subprocess
+
+# subprocess.run("aplay ./sounds/out.wav", shell=True)
 sf.write("./sounds/out.wav", y_i_samples, y.samplingRate)
 print("Showing all figures")
 frontend.primeTime() # Show all with blocking
-
-import subprocess
-
-subprocess.run("aplay ./sounds/out.wav", shell=True)
