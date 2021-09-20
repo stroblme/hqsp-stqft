3b28da56de0a0389a390cd1c78e44a994e86dc0d
stroblme <melvin.strobl@web.de>
2021-09-20 15:09:04 +0200

    disabled prints

--- sc_speechSignal_realDevice.py
+++ sc_speechSignal_realDevice.py
@@ -53,7 +53,7 @@ print("Processing STQFT")
 # device = "ibmq_melbourne"
 device = "ibmq_cambridge"
 
-stqft = transform(stqft_framework, suppressPrint=False, signalFilter=signalFilter, minRotation=minRotation, simulation=True, useNoiseModel=True, backendName=device, transpileOnce=True)
+stqft = transform(stqft_framework, suppressPrint=True, signalFilter=signalFilter, minRotation=minRotation, simulation=True, useNoiseModel=True, backendName=device, transpileOnce=True)
 y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
 y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale=scale, normalize=normalize, samplingRate=y.samplingRate, nMels=nMels, fmin=fmin, fmax=y.samplingRate/2)
 plotData = stqft.show(y_hat_stqft_p, f_p, t_p, subplot=[1,3,3], title="stqft_noise")
